6e0596840bd161095ed775e333d870c6c08c680a
SashaPorubaymikh <sasha095009@gmail.com>
2019-10-06 21:50:00 +0300

    Site more mobile-friendly. Weekdays translated

--- desktop/forms_logic.py
+++ desktop/forms_logic.py
@@ -1,12 +1,12 @@
 def get_weekdays_touple():
     """Returns weekdays tuple for choices in a form"""
     weekdays = [
-        (1, "Понедельник"),
-        (2, "Вторник"),
-        (3, "Среда"),
-        (4, "Четверг"),
-        (5, "Пятница"),
-        (6, "Суббота")
+        (1, "Monday"),
+        (2, "Tuesday"),
+        (3, "Wednesday"),
+        (4, "Thursday"),
+        (5, "Friday"),
+        (6, "Saturday")
     ]
     
     return weekdays
